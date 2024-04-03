0d6bc7a258858672b3e13080ce1d3f8d39921426
vklvykl <114586440+vklvykl@users.noreply.github.com>
2024-04-03 08:55:59 +0000

    7

--- inputs/config.py
+++ inputs/config.py
@@ -1,8 +1,8 @@
 CONVERT_ALL_TO_USDC = False  # convert all balances to USDC only
 
 THREADS = 1  # Enter amount of threads
-DELAY_BETWEEN_TRADE = (1200, 2400)  # Execute delay between every trade (Buy -> Delay -> Sell -> Buy -> Delay ...)
-DELAY_BETWEEN_DEAL = (1200, 2400)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
+DELAY_BETWEEN_TRADE = (1200, 3600)  # Execute delay between every trade (Buy -> Delay -> Sell -> Buy -> Delay ...)
+DELAY_BETWEEN_DEAL = (1200, 3600)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
 
 NEEDED_TRADE_VOLUME = 100000  # volume to trade, if 0 it will never stop
 MIN_BALANCE_TO_LEFT = 0  # min amount to left on the balance, if 0, it is traded until the balance is equal to 0.
